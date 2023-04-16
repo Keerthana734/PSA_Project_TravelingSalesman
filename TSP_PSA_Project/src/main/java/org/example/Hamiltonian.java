2d971c9cd15417cfd34950f71e58d7c2d29be624
keerthana <lnu.ke@northeastern.edu>
2023-04-15 21:06:45 -0400

    3 opt solution

--- TSP_PSA_Project/src/main/java/org/example/Hamiltonian.java
+++ TSP_PSA_Project/src/main/java/org/example/Hamiltonian.java
@@ -39,6 +39,7 @@ public class Hamiltonian {
         System.out.println("Hamiltonian cycle: " + String.join("->", hamiltonianCycle));
         System.out.println("Total distance covered: " + totalDistance);
         twoOpt(hamiltonianCycle, edgeWeight);
+        threeOpt(hamiltonianCycle,edgeWeight );
         // Return the Hamiltonian cycle
         return hamiltonianCycle;
     }
@@ -55,7 +56,6 @@ public class Hamiltonian {
                     if (newDistance < oldDistance) {
                         cycle = newCycle;
                         improved = true;
-                        System.out.println("2 opt 2 opt" + newDistance);
                     }
                 }
             }
@@ -104,6 +104,79 @@ public class Hamiltonian {
             totalDistance += edgeDistance;
         }
         return totalDistance;
+    }
+
+    public static List<String> threeOpt(List<String> tour, Map<String, Map<String, Double>> edgeWeight) {
+        boolean improvement = true;
+
+        while (improvement) {
+            improvement = false;
+
+            for (int i = 0; i < tour.size() - 2; i++) {
+                for (int j = i + 2; j < tour.size() - 1; j++) {
+                    for (int k = j + 2; k < tour.size(); k++) {
+                        List<String> newTour = reverseSubtour(tour, i + 1, j, k);
+                        double oldDistance = computeTourDistance(tour, edgeWeight);
+                        double newDistance = computeTourDistance(newTour, edgeWeight);
+
+                        if (newDistance < oldDistance) {
+                            tour = newTour;
+                            improvement = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        double totalDistance = 0;
+        for (int i = 0; i < tour.size() - 1; i++) {
+            String currVertex = tour.get(i);
+            String nextVertex = tour.get(i + 1);
+            Map<String, Double> vertexWeights = edgeWeight.get(currVertex);
+            Double edgedistance = vertexWeights.get(nextVertex);
+            if (edgedistance == null) {
+                // Handle the case where there is no edge between the current and next vertices
+                throw new IllegalArgumentException("No edge between " + currVertex + " and " + nextVertex);
+            }
+            totalDistance += edgedistance;
+        }
+
+        // Print the Hamiltonian cycle and the total distance covered in the cycle
+        System.out.println("3 opt cycle: " + String.join("->", tour));
+        System.out.println("Total distance covered 3 opt: " + totalDistance);
+
+        return tour;
+    }
+
+    private static List<String> reverseSubtour(List<String> tour, int i, int j, int k) {
+        List<String> newTour = new ArrayList<>(tour.subList(0, i));
+        newTour.addAll(reverseList(tour.subList(i, j + 1)));
+        newTour.addAll(tour.subList(j + 1, k));
+        newTour.addAll(reverseList(tour.subList(k, tour.size())));
+        return newTour;
+    }
+
+    private static List<String> reverseList(List<String> list) {
+        List<String> reversedList = new ArrayList<>(list);
+        Collections.reverse(reversedList);
+        return reversedList;
+    }
+
+    private static double computeTourDistance(List<String> tour, Map<String, Map<String, Double>> edgeWeight) {
+        double distance = 0;
+
+        for (int i = 0; i < tour.size() - 1; i++) {
+            String currVertex = tour.get(i);
+            String nextVertex = tour.get(i + 1);
+            Map<String, Double> vertexWeights = edgeWeight.get(currVertex);
+            Double edgeDistance = vertexWeights.get(nextVertex);
+            if (edgeDistance == null) {
+                return Double.MAX_VALUE;
+            }
+            distance += edgeDistance;
+        }
+
+        return distance;
     }
 
 }
