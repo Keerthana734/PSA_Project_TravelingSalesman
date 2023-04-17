8eebe7d3cd1299f969e5dbf7f99ecee0a8d3ced5
keerthana <lnu.ke@northeastern.edu>
2023-04-16 22:27:27 -0400

    removed reductant code

--- TSP_PSA_Project/src/main/java/org/example/ChristofidesAlgorithm.java
+++ TSP_PSA_Project/src/main/java/org/example/ChristofidesAlgorithm.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class ChristofidesAlgorithm {
 public static void main(String[] args){
-    Graph graph = GraphFromCSV.constructGraphFromCSV("crimeSample.csv");
+    Graph graph = GraphFromCSV.constructGraphFromCSV("info6205.spring2023.teamproject.csv");
     PrimsMST mst=new PrimsMST();
     MinimumWeightPerfectMatching.minimumWeightMatching((mst.primMST(graph)),graph,OddDegreeVertices.getOddDegreeVertices(mst.primMST(graph)));
 
