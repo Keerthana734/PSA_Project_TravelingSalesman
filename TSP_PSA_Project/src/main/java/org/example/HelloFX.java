d5e01b58683e7f388423a2b59d725ec31b43e4fd
PalakBanwari <banwari.p@northeastern.edu>
2023-04-16 22:09:29 -0400

    Added Graph and MST UI

--- TSP_PSA_Project/src/main/java/org/example/HelloFX.java
+++ TSP_PSA_Project/src/main/java/org/example/HelloFX.java
@@ -1,26 +1,150 @@
 
-
-
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.Group;
+import org.example.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+import java.util.ArrayList;
+import org.example.Edge;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 
 public class HelloFX extends Application {
 
+    private Map<String, CircleData> circleMap;
+
+    public HelloFX(){
+        circleMap = new HashMap<>();
+    }
+
+    public void addCircle(String name, double x, double y) {
+        CircleData circleData = new CircleData(name, x, y);
+        circleMap.put(name, circleData);
+    }
+
+    public CircleData getCircleData(String name) {
+        return circleMap.get(name);
+    }
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Random rand = new Random();
+        int i=250;
+
+        Graph graph = GraphFromCSV.constructGraphFromCSV("crimeSample.csv");
+        int count = graph.getNumVertices();
+        //Drawing a Circle
+        Group root = new Group();
+
+        StackPane p = new StackPane();
+        //String numstr;
+        List<String> verticesList = graph.getVertices();
+        for(String eachvertex: verticesList){
+            Circle circle = new Circle(10,Color.RED);
+            circle.setCenterX(rand.nextInt(110,1400));
+            circle.setCenterY(rand.nextInt(50,700));
+            circle.setAccessibleText(eachvertex);
+            root.getChildren().add(circle);
+
+        }
+
+
+        List<CircleData> circleDataList = new ArrayList<CircleData>();
+        for(Node node : root.getChildren()){
+            if (node instanceof Circle) {
+                Circle circle = (Circle) node;
+                CircleData circledata = new CircleData(circle.getAccessibleText(),circle.getCenterX(),circle.getCenterY());
+                circleDataList.add(circledata);
+            }
+        }
+
+        Button btMST = new Button("MST");
+
+
+        MSThandlerclass mstHandler = new MSThandlerclass(graph,circleDataList,root);
+        btMST.setOnAction(mstHandler);
+        root.getChildren().add(btMST);
+/*
+        PrimsMST mst=new PrimsMST();
+        List<Edge> mstEdgeList = mst.primMST(graph);
+        double startX = 0,startY=0 ,endX=0,endY=0;
+        for(Edge eachedge : mstEdgeList){
+            for(CircleData cdata: circleDataList){
+                if(cdata.getName() == eachedge.getSource()){
+                    startX = cdata.getX();
+                    startY = cdata.getY();
+                }
+                if(cdata.getName() == eachedge.getDestination()){
+                    endX = cdata.getX();
+                    endY = cdata.getY();
+                }
+
+            }
+            Line line = new Line(startX, startY, endX, endY);
+            root.getChildren().add(line);
+        }*/
+
+        //Creating a scene object
+        Scene scene = new Scene(root, 2000, 800);
+        //Setting title to the Stage
+        stage.setTitle("Christofides Algorithm");
+        //Adding scene to the stage
         stage.setScene(scene);
+        //Displaying the contents of the stage
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    class MSThandlerclass implements EventHandler<ActionEvent>{
+
+        private Graph graph;
+        private List<CircleData> circleDataList;
+        private Group root;
+                public MSThandlerclass(Graph graph,List<CircleData> circleDataList,Group root){
+                    this.graph = graph;
+                    this.circleDataList = circleDataList;
+                    this.root = root;
+
+                }
+        @Override
+        public void handle(ActionEvent event){
+
+            PrimsMST mst=new PrimsMST();
+            List<Edge> mstEdgeList = mst.primMST(graph);
+            double startX = 0,startY=0 ,endX=0,endY=0;
+            for(Edge eachedge : mstEdgeList){
+                for(CircleData cdata: circleDataList){
+                    if(cdata.getName() == eachedge.getSource()){
+                        startX = cdata.getX();
+                        startY = cdata.getY();
+                    }
+                    if(cdata.getName() == eachedge.getDestination()){
+                        endX = cdata.getX();
+                        endY = cdata.getY();
+                    }
+
+                }
+                Line line = new Line(startX, startY, endX, endY);
+                root.getChildren().add(line);
+            }
+
+
+        }
     }
 
+
+    public static void main(String args[]){
+        launch(args);
+    }
 }
+
+
