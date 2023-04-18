d35cd1d6211c6451795e574aff3cffe8f06ebc8b
PalakBanwari <banwari.p@northeastern.edu>
2023-04-18 17:50:51 -0400

    added ui changes

--- TSP_PSA_Project/src/main/java/org/example/TSPUI.java
+++ TSP_PSA_Project/src/main/java/org/example/TSPUI.java
@@ -58,7 +58,6 @@ public class TSPUI extends Application {
             circle.setCenterX(rand.nextInt(110,1400));
             circle.setCenterY(rand.nextInt(50,700));
             circle.setAccessibleText(eachvertex);
-
             root.getChildren().add(circle);
 
         }
@@ -76,6 +75,7 @@ public class TSPUI extends Application {
         Button btMST = new Button("MST");
         btMST.setLayoutX(0);
         btMST.setLayoutY(10);
+        btMST.setStyle("-fx-background-color: #DEB887");
         MSThandlerclass mstHandler = new MSThandlerclass(graph,circleDataList,root);
         btMST.setOnAction(mstHandler);
         root.getChildren().add(btMST);
@@ -84,41 +84,65 @@ public class TSPUI extends Application {
         Button btOddDegree = new Button("Odd Degree Vertices");
         btOddDegree.setLayoutX(50);
         btOddDegree.setLayoutY(10);
+        btOddDegree.setStyle("-fx-background-color: #DEB887");
         OddDegreehandlerclass OddDegreeHandler = new OddDegreehandlerclass(graph,circleDataList,root);
         btOddDegree.setOnAction(OddDegreeHandler);
         root.getChildren().add(btOddDegree);
 
+        //Eulerian Tour UI
+        Button btEulerianTour = new Button("Eulerian Tour");
+        btEulerianTour.setLayoutX(190);
+        btEulerianTour.setLayoutY(10);
+        btEulerianTour.setStyle("-fx-background-color: #DEB887");
+        EulerianTourhandlerclass EulerianTourHandler = new EulerianTourhandlerclass(graph,circleDataList,root);
+        btEulerianTour.setOnAction(EulerianTourHandler);
+        root.getChildren().add(btEulerianTour);
+
+
         //Christofide UI
         Button btHamiltonian = new Button("Christofides Algorithm");
-        btHamiltonian.setLayoutX(190);
+        btHamiltonian.setLayoutX(285);
         btHamiltonian.setLayoutY(10);
+        btHamiltonian.setStyle("-fx-background-color: #DEB887");
         Hamiltonianhandlerclass HamiltonianHandler = new Hamiltonianhandlerclass(graph,circleDataList,root);
         btHamiltonian.setOnAction(HamiltonianHandler);
         root.getChildren().add(btHamiltonian);
 
         //2-opt UI
         Button btTwoOpt = new Button("2-Opt");
-        btTwoOpt.setLayoutX(337);
+        btTwoOpt.setLayoutX(430);
         btTwoOpt.setLayoutY(10);
+        btTwoOpt.setStyle("-fx-background-color: #DEB887");
         TwoOpthandlerclass TwoOptHandler = new TwoOpthandlerclass(graph,circleDataList,root);
         btTwoOpt.setOnAction(TwoOptHandler);
         root.getChildren().add(btTwoOpt);
 
         //3-opt UI
         Button btThreeOpt = new Button("3-Opt");
-        btThreeOpt.setLayoutX(393);
+        btThreeOpt.setLayoutX(490);
         btThreeOpt.setLayoutY(10);
+        btThreeOpt.setStyle("-fx-background-color: #DEB887");
         ThreeOpthandlerclass ThreeOptHandler = new ThreeOpthandlerclass(graph,circleDataList,root);
         btThreeOpt.setOnAction(ThreeOptHandler);
         root.getChildren().add(btThreeOpt);
 
         //Simulated Annealing UI
         Button btsimulatedAnnealing = new Button("Simulated Annealing");
-        btsimulatedAnnealing.setLayoutX(450);
+        btsimulatedAnnealing.setLayoutX(550);
         btsimulatedAnnealing.setLayoutY(10);
+        btsimulatedAnnealing.setStyle("-fx-background-color: #DEB887");
         simulatedAnnealinghandlerclass simulatedAnnealingHandler = new simulatedAnnealinghandlerclass(graph,circleDataList,root);
         btsimulatedAnnealing.setOnAction(simulatedAnnealingHandler);
         root.getChildren().add(btsimulatedAnnealing);
+
+        //Ant Colony Optimization UI
+        Button btAntColony = new Button("Ant Colony Optimization");
+        btAntColony.setLayoutX(685);
+        btAntColony.setLayoutY(10);
+        btAntColony.setStyle("-fx-background-color: #DEB887");
+        antcolonyhandlerclass antcolonyHandler = new antcolonyhandlerclass(graph,circleDataList,root);
+        btAntColony.setOnAction(antcolonyHandler);
+        root.getChildren().add(btAntColony);
 
         //Creating a scene object
         Scene scene = new Scene(root, 2000, 800);
@@ -128,6 +152,66 @@ public class TSPUI extends Application {
         stage.setScene(scene);
         //Displaying the contents of the stage
         stage.show();
+
+    }
+
+    class EulerianTourhandlerclass implements EventHandler<ActionEvent> {
+
+        private Graph graph;
+        private List<CircleData> circleDataList;
+        private Group root;
+
+        EulerianTourhandlerclass(Graph graph,List<CircleData> circleDataList,Group root){
+            this.graph = graph;
+            this.circleDataList = circleDataList;
+            this.root = root;
+        }
+
+        @Override
+        public void handle(ActionEvent event){
+
+            ArrayList<Node> linesToRemove = new ArrayList<>();
+
+            for (Node node : root.getChildren()) {
+                if (node instanceof Line) {
+                    linesToRemove.add(node);
+                }
+            }
+            if(linesToRemove!=null){
+                root.getChildren().removeAll(linesToRemove);
+            }
+            PrimsMST mst=new PrimsMST();
+            Graph H = MinimumWeightPerfectMatching.minimumWeightMatching((mst.primMST(graph)),graph,OddDegreeVertices.getOddDegreeVertices(mst.primMST(graph)));
+            Map<String, Map<String, Double>> edgeWeight = graph.getEdgeWeight();
+            List<String> tour = HierholzerAlgorithm.findEulerTour(H);
+            List<String> eulerTour = Hamiltonian.shortcutEulerianCycle(tour,edgeWeight);
+
+
+
+
+            //List<String> hamiltonianCycle = MinimumWeightPerfectMatching.minimumWeightMatching((PrimsMST.primMST(graph)),graph,OddDegreeVertices.getOddDegreeVertices(PrimsMST.primMST(graph)));
+
+            double startX ,startY,endX,endY;
+            for(int i=0;i< eulerTour.size()-1;i++){
+                startX = 0;startY=0 ;endX=0;endY=0;
+
+                for(CircleData eachCircleData : circleDataList){
+                    if(eulerTour.get(i)==eachCircleData.getName()) {
+                        startX = eachCircleData.getX();
+                        startY = eachCircleData.getY();
+                    }
+                    if(eulerTour.get(i+1)==eachCircleData.getName()){
+                        endX= eachCircleData.getX();
+                        endY=eachCircleData.getY();
+
+                    }
+                }
+                Line line = new Line(startX, startY, endX, endY);
+                line.setStroke(Color.DARKMAGENTA);
+                root.getChildren().add(line);
+            }
+
+        }
 
     }
 
@@ -368,6 +452,67 @@ public class TSPUI extends Application {
                 }
                 Line line = new Line(startX, startY, endX, endY);
                 line.setStroke(Color.ORANGE);
+                root.getChildren().add(line);
+            }
+
+        }
+
+    }
+
+    class antcolonyhandlerclass implements EventHandler<ActionEvent> {
+
+        private Graph graph;
+        private List<CircleData> circleDataList;
+        private Group root;
+
+        antcolonyhandlerclass(Graph graph,List<CircleData> circleDataList,Group root){
+            this.graph = graph;
+            this.circleDataList = circleDataList;
+            this.root = root;
+        }
+
+        @Override
+        public void handle(ActionEvent event){
+
+            ArrayList<Node> linesToRemove = new ArrayList<>();
+            for (Node node : root.getChildren()) {
+                if (node instanceof Line) {
+                    linesToRemove.add(node);
+                }
+            }
+            if(linesToRemove!=null){
+                root.getChildren().removeAll(linesToRemove);
+            }
+
+            // List<String> hamiltonianCycle = MinimumWeightPerfectMatching.minimumWeightMatching((PrimsMST.primMST(graph)),graph,OddDegreeVertices.getOddDegreeVertices(PrimsMST.primMST(graph)));
+            // Map<String, Map<String, Double>> edgeWeight = graph.getEdgeWeight();
+            // List<String> threeOptRoute = Hamiltonian.threeOpt(hamiltonianCycle, edgeWeight);
+
+            PrimsMST mst=new PrimsMST();
+            Graph H = MinimumWeightPerfectMatching.minimumWeightMatching((mst.primMST(graph)),graph,OddDegreeVertices.getOddDegreeVertices(mst.primMST(graph)));
+            Map<String, Map<String, Double>> edgeWeight = graph.getEdgeWeight();
+            List<String> tour = HierholzerAlgorithm.findEulerTour(H);
+            List<String> eulerTour = Hamiltonian.shortcutEulerianCycle(tour,edgeWeight);
+            List<String> hamiltonianCycle = Hamiltonian.shortcutEulerianCycle(eulerTour,edgeWeight);
+            List<String> threeOptRoute = simulatedAnnealing(hamiltonianCycle,edgeWeight);
+
+            double startX ,startY,endX,endY;
+            for(int i=0;i< threeOptRoute.size()-1;i++){
+                startX = 0;startY=0 ;endX=0;endY=0;
+
+                for(CircleData eachCircleData : circleDataList){
+                    if(threeOptRoute.get(i)==eachCircleData.getName()) {
+                        startX = eachCircleData.getX();
+                        startY = eachCircleData.getY();
+                    }
+                    if(threeOptRoute.get(i+1)==eachCircleData.getName()){
+                        endX= eachCircleData.getX();
+                        endY=eachCircleData.getY();
+
+                    }
+                }
+                Line line = new Line(startX, startY, endX, endY);
+                line.setStroke(Color.AQUAMARINE);
                 root.getChildren().add(line);
             }
 
